c3194a29a298c395796cc264268f7e4e55e1294c
svwielga4@gmail.com <svwielga4@gmail.com>
2018-01-09 13:14:58 -0800

    editing format

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,47 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POST = 'CREATE_POST';
-export const FETCH_POST = 'FETCH_POST';
-export const DELETE_POST = 'DELETE_POST';
+export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POST = "CREATE_POST";
+export const FETCH_POST = "FETCH_POST";
+export const DELETE_POST = "DELETE_POST";
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
-const API_KEY = '?key=eer3r22WEv';
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = "?key=eer3r22WEv";
 
-export function fetchPosts(){
-    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
-    return{
-        type: FETCH_POSTS,
-        payload: request
-    }
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
 }
 
-export function createPost(values, callback){
-    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
-        .then(() => callback());
+export function createPost(values, callback) {
+  const request = axios
+    .post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
 
-    return {
-        type: CREATE_POST,
-        payload: request
-    }
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
 }
 
-export function fetchPost(id){
-    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
 
-    return{
-        type: FETCH_POST,
-        payload: request
-    }
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
 }
 
-export function deletePost(id, callback){
-    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
-        .then(() => callback());
+export function deletePost(id, callback) {
+  const request = axios
+    .delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
 
-    return{
-        type: DELETE_POST,
-        payload: id
-    }
+  return {
+    type: DELETE_POST,
+    payload: id
+  };
 }
